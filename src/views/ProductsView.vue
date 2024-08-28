0d18bca44c4c5ff6e174e84296f7566eee3730ed
Emmanuel <emmanuelmathew1995@gmail.com>
2024-08-29 00:01:51 +0530

    Enhance LoginView with responsive padding and update ProductsView with improved filter and pagination UI/UX

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -129,13 +129,16 @@
               </li>
             </ul>
           </div>
-          <div class="flex justify-between items-center mb-4">
+          <div class="flex justify-between items-center mb-6">
             <p class="text-gray-600 dark:text-gray-400">
               Showing {{ filteredProducts.length > 0 ? (currentPage - 1) * itemsPerPage + 1 : 0 }} - 
               {{ Math.min(currentPage * itemsPerPage, filteredProducts.length) }} 
               of {{ filteredProducts.length }} product{{ filteredProducts.length !== 1 ? 's' : '' }}
             </p>
-            <button @click="resetFilters" class="text-indigo-600 hover:text-indigo-800 dark:text-indigo-400 dark:hover:text-indigo-200 transition duration-300">
+            <button @click="resetFilters" class="flex items-center px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 shadow-md">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
+              </svg>
               Reset Filters
             </button>
           </div>
